00bb13755a0601f1b86c4f7e945054e226bbbe73
Tech-A38 <Tech-A38@Tech-A38s-MacBook-Air.local>
2016-11-25 09:55:34 +0000

    first commit

---
+++ js/ownJavascript.js
@@ -0,0 +1,16 @@
+
+// $(function (){
+//     console.log('Loaded');
+//     var options = [
+
+//     {selector: '#ulJobs', offset: 400, callback: function(el) {
+//       Materialize.showStaggeredList($(el));
+//     } }
+
+//   ];
+//   Materialize.scrollFire(options);
+// });
+    
+    $(document).ready(function(){
+      $('.parallax').parallax();
+    });
