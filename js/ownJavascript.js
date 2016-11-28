a516bec35b25fe37e74e713c3be30c19371ffc26
Tech-A38 <Tech-A38@Tech-A38s-MacBook-Air.local>
2016-11-28 17:06:15 +0000

    Added CSS and Javascript

--- js/ownJavascript.js
+++ js/ownJavascript.js
@@ -1,16 +1,52 @@
+$(document).ready(function(){
+    // Parallax
+    $('.parallax').parallax();
+    
+});
 
-// $(function (){
-//     console.log('Loaded');
-//     var options = [
+$(function(){
+    
+    $personalDetails = $('.personalDetails');
+    $jobs = $('.jobs');
+    $education = $('.education');
+    
+    var options = [
 
-//     {selector: '#ulJobs', offset: 400, callback: function(el) {
-//       Materialize.showStaggeredList($(el));
-//     } }
+    {selector: '.personalDetails', offset: 200, callback: '$personalDetails.addClass("bounceInRight animated").css("visibility", "visible")'},   
+    {selector: '.education', offset: 200, callback: '$education.addClass("bounceInLeft animated").css("visibility", "visible")'},
+    {selector: '.jobs', offset: 200, callback: '$jobs.addClass("bounceInRight animated").css("visibility", "visible")'}   
+  ];
+
+  Materialize.scrollFire(options);
+
+});
+
+//     $boxTwo = $('.box');
+
+// $('.toggleButton').on('click', function() { 
+//   if($(this).html() === 'Play') 
+//   {
+//     $(this).html('Pause');
+//     $boxTwo.addClass('horizTranslate');
+//   } else {
+//     $(this).html('Play');
+//     var computedStyle = $boxTwo.css('margin-left');
+//     $boxTwo.removeClass('horizTranslate');
+//     $boxTwo.css('margin-left', computedStyle);
+//   }  
+// });
 
 //   ];
 //   Materialize.scrollFire(options);
+
+//     if($(this).html() === 'Play') 
+//     {
+//         $(this).html('Pause');
+//         $boxTwo.addClass('horizTranslate');
+//     } else {
+//         $(this).html('Play');
+//         var computedStyle = $boxTwo.css('margin-left');
+//         $boxTwo.removeClass('horizTranslate');
+//         $boxTwo.css('margin-left', computedStyle);
+//     }      
 // });
-    
-    $(document).ready(function(){
-      $('.parallax').parallax();
-    });
