5c8a3e3d95a6a82b3df964596cba5834a6bea0b3
Tech-A38 <Tech-A38@Tech-A38s-MacBook-Air.local>
2016-11-29 15:00:17 +0000

    Added more CSS and Javascript

--- js/ownJavascript.js
+++ js/ownJavascript.js
@@ -1,52 +1,57 @@
 $(document).ready(function(){
     // Parallax
     $('.parallax').parallax();
+
+    $(document).ready(function(){
+      $('.carousel').carousel();
+    });
+
+    $(function(){
+        
+        $personalDetails = $('.personalDetails');
+        $jobs = $('.jobs');
+        $education = $('.education');
+        
+        var options = [
+
+        {selector: '.personalDetails', offset: 200, callback: '$personalDetails.addClass("bounceInRight animated").css("visibility", "visible")'},   
+        {selector: '.education', offset: 200, callback: '$education.addClass("bounceInLeft animated").css("visibility", "visible")'},
+        {selector: '.jobs', offset: 200, callback: '$jobs.addClass("bounceInRight animated").css("visibility", "visible")'}   
+    ];
+
+    Materialize.scrollFire(options);
+
+    });
     
+    $(function() {
+
+        $('a[href*="#"]:not([href="#"])').click(function() {
+
+            if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+            
+            var target = $(this.hash);
+            target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+            if (target.length) {
+                
+                $('html, body').animate({
+                scrollTop: target.offset().top
+                }, 1000);
+                return false;
+            }
+            }
+        });
+    });
+    
+    $(function(){
+
+        $("#nav-mobile a").click(function(){
+        console.log("hello");
+        $("#nav-mobile").find(".active").removeClass("active");
+        $(this).parent().addClass("active");
+        });
+    });
+
 });
 
-$(function(){
-    
-    $personalDetails = $('.personalDetails');
-    $jobs = $('.jobs');
-    $education = $('.education');
-    
-    var options = [
 
-    {selector: '.personalDetails', offset: 200, callback: '$personalDetails.addClass("bounceInRight animated").css("visibility", "visible")'},   
-    {selector: '.education', offset: 200, callback: '$education.addClass("bounceInLeft animated").css("visibility", "visible")'},
-    {selector: '.jobs', offset: 200, callback: '$jobs.addClass("bounceInRight animated").css("visibility", "visible")'}   
-  ];
 
-  Materialize.scrollFire(options);
-
-});
-
-//     $boxTwo = $('.box');
-
-// $('.toggleButton').on('click', function() { 
-//   if($(this).html() === 'Play') 
-//   {
-//     $(this).html('Pause');
-//     $boxTwo.addClass('horizTranslate');
-//   } else {
-//     $(this).html('Play');
-//     var computedStyle = $boxTwo.css('margin-left');
-//     $boxTwo.removeClass('horizTranslate');
-//     $boxTwo.css('margin-left', computedStyle);
-//   }  
-// });
-
-//   ];
-//   Materialize.scrollFire(options);
-
-//     if($(this).html() === 'Play') 
-//     {
-//         $(this).html('Pause');
-//         $boxTwo.addClass('horizTranslate');
-//     } else {
-//         $(this).html('Play');
-//         var computedStyle = $boxTwo.css('margin-left');
-//         $boxTwo.removeClass('horizTranslate');
-//         $boxTwo.css('margin-left', computedStyle);
-//     }      
-// });
