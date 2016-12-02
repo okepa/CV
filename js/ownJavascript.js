702ddc99f11ebeb1c81a9a1eb095589d6d413697
Tech-A38 <Tech-A38@rich_0115m.uk.brad-ex.co.uk>
2016-12-02 10:49:56 +0000

    added more to CV

--- js/ownJavascript.js
+++ js/ownJavascript.js
@@ -1,22 +1,22 @@
 $(document).ready(function(){
+    //Slider
+    $('.slider').slider({full_width: true});
+    $('.slider').slider({height: 690});
+
+   
     // Parallax
     $('.parallax').parallax();
-
-    $(document).ready(function(){
-      $('.carousel').carousel();
-    });
+    //Carousel
+    $('.carousel').carousel();
 
     $(function(){
-        
-        $personalDetails = $('.personalDetails');
-        $jobs = $('.jobs');
-        $education = $('.education');
-        
+                
         var options = [
 
-        {selector: '.personalDetails', offset: 200, callback: '$personalDetails.addClass("bounceInRight animated").css("visibility", "visible")'},   
-        {selector: '.education', offset: 200, callback: '$education.addClass("bounceInLeft animated").css("visibility", "visible")'},
-        {selector: '.jobs', offset: 200, callback: '$jobs.addClass("bounceInRight animated").css("visibility", "visible")'}   
+        {selector: '.personalDetails', offset: 200, callback: '$(".personalDetails").addClass("bounceInRight animated").css("visibility", "visible")'},   
+        {selector: '.education', offset: 200, callback: '$(".education").addClass("bounceInLeft animated").css("visibility", "visible")'},
+        {selector: '.jobs', offset: 200, callback: '$(".jobs").addClass("bounceInRight animated").css("visibility", "visible")'},
+        {selector: '.skills', offset: 200, callback: '$(".skills").addClass("bounceInLeft animated").css("visibility", "visible")'}    
     ];
 
     Materialize.scrollFire(options);
@@ -31,11 +31,17 @@ $(document).ready(function(){
             
             var target = $(this.hash);
             target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+
+
+
             if (target.length) {
-                
+                //Moves the body
                 $('html, body').animate({
-                scrollTop: target.offset().top
+                //gets the position of the element
+                scrollTop: target.offset().top - 50
                 }, 1000);
+                console.log(target.offset().top);
+                console.log(target.offset().top - 50);
                 return false;
             }
             }
@@ -45,7 +51,6 @@ $(document).ready(function(){
     $(function(){
 
         $("#nav-mobile a").click(function(){
-        console.log("hello");
         $("#nav-mobile").find(".active").removeClass("active");
         $(this).parent().addClass("active");
         });
